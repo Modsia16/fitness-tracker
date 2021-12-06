3a1fc66f7ea5bf7b33207b47eb028bb691dde909
Modsia16 <84951325+Modsia16@users.noreply.github.com>
2021-12-05 20:53:49 -0800

    server change

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const logger = require("morgan");
+//const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3003;
 
 const app = express();
 
-app.use(logger("dev"));
+//app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,7 +14,7 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useFindAndModify: false
 });
 
 //require(apiRoute)routes;
