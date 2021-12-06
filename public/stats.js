e1e3b2ac84401ec49486846bed89b7e9732d96b5
Modsia16 <84951325+Modsia16@users.noreply.github.com>
2021-12-05 20:02:20 -0800

    added indexs change

--- public/stats.js
+++ public/stats.js
@@ -111,4 +111,3 @@ function populateChart(data) {
 API.getWorkoutsInRange().then(populateChart);
   
   
-  
--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,46 +1,45 @@
 const router = require("express").Router();
 const db = require("../models");
 
-  router.get("/api/workouts", (req, res) => {
-    db.Workout.find({}).then(dbWorkout => {
-      dbWorkout.forEach(workout => {
-        var total = 0;
-        workout.exercises.forEach(exercise => {
-          total += exercise.duration;
+  router.get("/workouts", (req, res) => {
+    db.Workout.find({})
+    .sort({ date: -1 })
+    .then(dbWorkout => {
+        res.status(200).json(dbWorkout)
     })
-    workout.totalDuration = total;
-    }); res.json(dbWorkout);
-  })
     .catch(err => {
-      res.status(400).json(err);
-    });
-  });
+        res.status(400).json(err)
+    })
+})
   
-  router.put("/api/workouts/:id", (req, res) => {
-    db.Workout.findByIdAndUpdate(
-      { _id: req.params.id }, { 
-        $push: { exercises: req.body },
-        $inc: { totalDuration: req.body.duration }
-      },
-      { new: true }).then((dbWorkout) => {
-        res.json(dbWorkout);
-      }).catch(err => {
-        res.status(400).json(err);
-      });
-    });
+  router.put("/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(req.params.id, 
+    { $push: { exercises: req.body } })
+		.then((data) => {
+			res.json(data);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
     
-      router.post("/api/workouts", ({ body }, res) => {
-        db.Workout.create(body).then((dbWorkout => {
-          res.json(dbWorkout);
-        })).catch(err => {
+      router.post("/workouts", (req, res) => {
+        db.Workout.create(req.body)
+        .then((workout) => {
+          res.json(workout);
+        })
+        .catch((err) => {
           res.status(400).json(err);
         });
-      });
+    });
     
-    router.get("/api/workouts/range", (req, res) => {
-      db.Workout.find({}).then((dbWorkout => {
-        res.json(dbWorkout);
-      })).catch(err => {
+    router.get("/workouts/range", (req, res) => {
+      db.Workout.find({})
+      .sort({ date: -1 })
+      .then((workout) => {
+        res.status(200).json(workout);
+      })
+      .catch((err) => {
         res.status(400).json(err);
       });
   });
