6d417349cc4e6cbd80125901306f0a1c8b904341
Modsia16 <84951325+Modsia16@users.noreply.github.com>
2021-12-05 18:56:42 -0800

    added additional app code

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 module.exports = function(app) {
 
-  router.get("/", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
   
